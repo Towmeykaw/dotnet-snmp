dd3caf6d3dbb3ad5e15e2563f784d17570d03f04
towmeykaw <tkindmark@gmail.com>
2019-03-31 22:09:29 +0200

    still testing

---
+++ snmp-sender/SnmpHelpers.cs
@@ -0,0 +1,17 @@
+﻿using Lextm.SharpSnmpLib;
+namespace SnmpSender
+{
+    public static class SnmpHelpers
+    {
+        public static VersionCode GetVersion(int version)
+        {
+            return version switch
+            {
+                1 => VersionCode.V1,
+                2 => VersionCode.V2,
+                3 => VersionCode.V3,
+                _ => VersionCode.V2
+            };
+        }
+    }
+}
